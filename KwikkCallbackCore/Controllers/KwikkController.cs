f087818ac7bb93c55998c26a99bf6c8b706f1674
mattias lundh <mattiasanderslundh@gmail.com>
2021-10-28 19:21:42 +0200

    initial

---
+++ KwikkCallbackCore/Controllers/KwikkController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KwikkCallbackCore;
+using System;
+using System.Linq;
+
+namespace KwikkCallbackCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KwikkController : ControllerBase
+    {
+        [HttpPost]
+        [Route("callback")]
+        public ActionResult Callback(CallbackDto callbackDto)
+        {
+            // ToDo: interesting things with callbackDto object
+
+            return Ok("I saw a callback and it was good");
+        }
+    }
+}
